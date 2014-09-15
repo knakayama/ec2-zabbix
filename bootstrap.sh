178d81729fae7a7f069966b185fc1da4ebba5500
knakayama <knakayama.sh@gmail.com>
2014-09-15 21:31:00 +0900

    add zabbix-agent role

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ knife bootstrap "$HOSTNAME" \
     --ssh-user "$AWS_SSH_USERNAME" \
     --node-name "$HOSTNAME" \
     --identity-file "$AWS_SSH_KEY_PATH" \
-    --run-list "role[chef-client],role[zabbix-server]" \
+    --run-list "role[chef-client],role[zabbix-server],role[zabbix-agent]" \
     --sudo
 
