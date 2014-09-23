6635b1477e5b3c9eb336a7d1aa0eb82adf528fc3
knakayama <knakayama.sh@gmail.com>
2014-09-23 20:01:15 +0900

    fix variable name

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 knife bootstrap "$HOSTNAME" \
     --ssh-user "$AWS_SSH_USERNAME" \
-    --node-name "$HOSTNAME" \
+    --node-name "$HOST_NAME" \
     --identity-file "$AWS_SSH_KEY_PATH" \
     --run-list "role[chef-client],role[zabbix-server],role[zabbix-agent]" \
     --sudo
